153704623de32196ae691372398c4feb547901a6
Edwin Vesga <43819230+Vesgae@users.noreply.github.com>
2022-10-12 12:24:31 -0500

    FIRST COMMIT - MS books

---
+++ src/main/java/com/web202230/ms_books/MsBooksApplication.java
@@ -0,0 +1,13 @@
+package com.web202230.ms_books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web202230/ms_books/MsBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web202230.ms_books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
